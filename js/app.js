7f4fa27854783caf6494d9d993fd1a30b0827d7d
Darius Burgan <darius@dariusburgan.com>
2018-06-04 21:16:36 -0400

    Improve comments

--- js/app.js
+++ js/app.js
@@ -5,11 +5,13 @@ class MovableThing{
         this.y = y;
     }
 
+    // Draw the enemy on the screen, required method for game
     render() {
         ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
     }
 }
 
+// Enemies the player must avoid
 class Enemy extends MovableThing{
     constructor(x, y, speed){
         super(x, y);
@@ -17,6 +19,8 @@ class Enemy extends MovableThing{
         this.sprite = 'images/enemy-bug.png'; // Charavter image
     }
 
+    // Update the enemy's position, required method for game
+    // Parameter: dt, a time delta between ticks
     update(dt){
         // If enemy hits player, decrease score by 5, update score on screen, and reset player to starting locations
         if (Math.abs(player.x - this.x) < 50 && Math.abs(player.y - this.y) < 50) {
@@ -111,134 +115,6 @@ class Player extends MovableThing{
         this.y = 400;
     }
 }
-
-/*
-// Enemies the player must avoid
-const Enemy = function(x, y, speed) {
-    // Starting location variables
-    this.x = x;
-    this.y = y;
-
-    // How fast the enemy will move
-    this.speed = speed;
-
-    //Enemy graphic
-    this.sprite = 'images/enemy-bug.png';
-};
-
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
-
-    // If enemy hits player, decrease score by 5, update score on screen, and reset player to starting locations
-    if (Math.abs(player.x - this.x) < 50 && Math.abs(player.y - this.y) < 50) {
-        if (player.currentScore > 0) {
-            player.currentScore -= 5;
-        }
-        player.updateScoreboard();
-        player.reset();
-    }
-
-    // Moves the enemy across the screen
-    this.x += this.speed * dt;
-
-    // When enemy is off screen, loop him back to beginning of board.
-    if (this.x > 505) {
-        this.x = Math.ceil((Math.random() * -300) - 100);
-    }
-
-};
-
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};*/
-
-/*
-// Player class
-const Player = function(x, y) {
-    // Player starting location
-    this.x = x;
-    this.y = y;
-
-    // Variable for keeping score
-    this.currentScore = 0;
-
-    // Player graphic
-    this.sprite = 'images/char-boy.png';
-};
-
-Player.prototype.update = function(dt) {
-    // Once player reaches water, add 10 points to his score, and then reset his position
-    if (this.y < 20) {
-        this.currentScore += 10;
-        this.updateScoreboard();
-        this.reset();
-    }
-
-    // Modal congratulating player once he wins the game (30 points)
-    if (this.currentScore >= 30) {
-        //alert('Congratulations you won!')
-
-        // Select the modal
-        const modalPopup = document.querySelector('.modal');
-
-        // Select the modal's close button
-        const modalCloseBtn = document.querySelector('.close');
-
-        // Select the modal's paragraph tag
-        const modalParagraph = document.querySelector('p');
-
-         // Add content to modal paragraph
-         modalParagraph.innerHTML = `Congratulations you won!`;
-
-        // Select the restart game button
-        const restartGameModalButton = document.querySelector('.restartbtn');
-
-        // Make modal visible
-        modalPopup.style.display = "block";
-
-        // Close modal
-        const closeModal = () => {
-            modalPopup.style.display = "none";
-        }
-
-        restartGameModalButton.addEventListener('click', () => {
-            location.reload(true);
-        })
-    }
-};
-
-Player.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
-
-Player.prototype.handleInput = function(arrow) {
-    // Moves the player based on keyboard arrow pressed. Also prevents the player from being moved off screen
-    if (arrow === 'left' && this.x > 80) {
-        this.x -= 100;
-    } else if (arrow === 'up' && this.y > 20) {
-        this.y -= 90;
-    } else if (arrow === 'right' && this.x < 400) {
-        this.x += 100;
-    } else if (arrow === 'down' && this.y < 350) {
-        this.y += 90;
-    }
-};
-
-// Function that updates the score
-Player.prototype.updateScoreboard = function() {
-    const scoreboard = document.querySelector('.score');
-
-    scoreboard.innerHTML = this.currentScore;
-}
-
-Player.prototype.reset = function() {
-    // Changes player's location
-    this.x = 200;
-    this.y = 400;
-};
-*/
 
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
